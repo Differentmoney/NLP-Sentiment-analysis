95f74565c1894be80678d38b7b5682c8228993c2
axiao <56705341+Differentmoney@users.noreply.github.com>
2022-04-02 13:32:15 -0400

    Removed uneeded packages

--- main.py
+++ main.py
@@ -2,10 +2,6 @@ import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as plt 
 import seaborn as sns
-import re
-import time
-import string
-import warnings
 
 import sentiment_analysis as pp
 
